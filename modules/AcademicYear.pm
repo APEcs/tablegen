3d58848fb8c784f0de671ca3c8454a8b800ed407
Chris <chris@starforge.co.uk>
2014-08-08 14:47:05 +0100

    Loading and conversion of academic year information.

---
+++ modules/AcademicYear.pm
@@ -0,0 +1,173 @@
+## @file
+# This file contains the implementation of the Academic Year model
+#
+# @author  Chris Page &lt;chris@starforge.co.uk&gt;
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## @class
+#
+package AcademicYear;
+
+use v5.12;
+use base qw(Webperl::SystemModule);
+use DateTime;
+use XML::Simple;
+
+# ============================================================================
+#  Constructor
+
+## @cmethod $ new(%args)
+# Construct a new AcademicYear model object to handle loading of year
+# data and lookup of week information.
+#
+# @param args A hash of values to initialise the object with.
+# @return A new AcademicYear object.
+sub new {
+    my $invocant = shift;
+    my $class    = ref($invocant) || $invocant;
+    my $self     = $class -> SUPER::new(minimal    => 1, # minimal tells SystemModule to skip object checks
+                                        fix_breaks => 1, # if set to zero, adjustments to break start/end times are not made
+                                        yearfile   => '',
+                                        @_)
+        or return undef;
+
+    # load the year file if possible
+    $self -> load_yearfile()
+        if($self -> {"yearfile"});
+
+    return $self;
+}
+
+
+# ============================================================================
+#  Year loader and access
+
+## @method $ load_yearfile($filename)
+# Attempt to populate the AcademicYear object's information about when semesters
+# and breaks start and end for each year based on the information in the
+# specified year file.
+#
+# @param yearfile The name of the year file to load the academic year information
+#                 from. If not specified, the yearfile set when creating the
+#                 object is used instead.
+# @return true on success, undef on error. If this returns undef, but a previous
+#         call returned true, the last successfully loaded information is not
+#         cleared.
+sub load_yearfile {
+    my $self     = shift;
+    my $filename = shift || $self -> {"yearfile"};
+
+    $self -> clear_error();
+
+    return $self -> self_error("No year file specified")
+        if(!$filename);
+
+    my $yeardata = eval { XMLin($filename, KeepRoot => 0, ForceArray => [ 'break' ]) };
+    return $self -> self_error("Year file loading failed: $@")
+        if($@);
+
+    $self -> {"yeardata"} = $yeardata;
+    $self -> {"yearfile"} = $filename;
+
+    # Now fix up all the dates
+    return $self -> _convert_yeardata_dates();
+}
+
+
+# ============================================================================
+#  Internal implementation stuff
+
+
+## @method private $ _convert_yeardata_dates(void)
+# Convert the date fields in the yeardata to DateTime objects if they are not
+# already in that format.
+#
+# @return true on success, undef on error
+sub _convert_yeardata_dates {
+    my $self  = shift;
+    my $years = $self -> {"yeardata"} -> {"year"}; # For laziness.
+
+    # Years are hashed by their ID
+    foreach my $year (keys(%{$years})) {
+
+        # Years can contain multiple semesters
+        foreach my $semester (keys(%{$years -> {$year} -> {"semester"}})) {
+            my $semref = $years -> {$year} -> {"semester"} -> {$semester};
+
+            # Convert the start and end dates for the semester
+            $semref -> {"start"} = $self -> _convert_date($semref -> {"start"})
+                or return undef;
+            $semref -> {"end"}   = $self -> _convert_date($semref -> {"end"}, 1)
+                or return undef;
+
+            # Semesters may contain breaks
+            foreach my $break (keys(%{$semref -> {"break"}})) {
+                my $breakref = $semref -> {"break"} -> {$break};
+
+                $breakref -> {"start"} = $self -> _convert_date($breakref -> {"start"})
+                    or return undef;
+                $breakref -> {"end"}   = $self -> _convert_date($breakref -> {"end"}, 1)
+                    or return undef;
+
+                # Fix up start and end if needed
+                if($self -> {"fix_breaks"}) {
+                    $breakref -> {"start"} -> add(days => 1);
+                    $breakref -> {"end"} -> subtract(days => 1);
+                }
+            }
+        }
+    }
+
+    return 1;
+}
+
+
+## @method private $ _convert_date($date, $end)
+# If the specified date is not a DateTime object, attempt to use it as an
+# ISO8601 date to create a DateTime object from.
+#
+# @param date The date to create a DateTime object from.
+# @param end  If true, the time is set to 23:59:59 rather than 00:00:00.
+# @return A reference to a DateTime object on success, undef on error.
+sub _convert_date {
+    my $self = shift;
+    my $date = shift;
+    my $end  = shift;
+
+    $self -> clear_error();
+
+    # Pass DateTime objects through untouched
+    return $date
+        if(ref($date) && $date -> isa("DateTime"));
+
+    return $self -> self_error("Illegal value passed to _convert_date")
+        if(ref($date));
+
+    my ($year, $mon, $day) = $date =~ /^(\d{4})-?(\d{2})-?(\d{2})$/;
+    return $self -> self_error("Unable to parse date string from '$date'")
+        if(!$year || !$mon || !$day);
+
+
+    my $datetime = eval { DateTime -> new(year => $year, month => $mon, day => $day) };
+    return $self -> self_error("Unable to create DateTime object: $@")
+        if($@);
+
+    $datetime -> set(hour => 23, minute => 59, second => 59)
+        if($end);
+
+    return $datetime;
+}
+
+1;
