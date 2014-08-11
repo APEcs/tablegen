3dd51825f15d96e126e765410fdc7425d603a2c3
Chris <chris@starforge.co.uk>
2014-08-11 15:56:50 +0100

    Working on generation

--- modules/TableGen.pm
+++ modules/TableGen.pm
@@ -39,12 +39,16 @@ sub new {
     my $class    = ref($invocant) || $invocant;
     my $self     = $class -> SUPER::new(minimal  => 1, # minimal tells SystemModule to skip object checks
                                         course   => '',
-                                        basepath => '',
+                                        datapath => '',
                                         @_)
         or return undef;
 
-    # must have an academic year object
+    # must have academic year and template objects
+    return Webperl::SystemModule::set_error("No Template object available.") if(!$self -> {"template"});
     return Webperl::SystemModule::set_error("No AcademicYear object available.") if(!$self -> {"acyear"});
+
+    $self -> load_course()
+        if($self -> {"course"});
 
     return $self;
 }
@@ -79,6 +83,36 @@ sub load_course {
 }
 
 
+
+sub generate {
+    my $self = shift;
+    my $year = shift;
+
+    $self -> clear_error();
+
+    # Can't do anything if the content isn't loaded
+    return $self -> self_error("No course definition information loaded")
+        if(!$self -> {"coursedef"});
+
+    my $header = $self -> _build_header($self -> {"coursedef"} -> {"table"} -> {"columns"} -> {"column"});
+    my $body   = $self -> _build_body($self -> {"coursedef"} -> {"table"} -> {"columns"} -> {"column"},
+                                      $self -> {"coursedef"} -> {"table"} -> {"rows"} -> {"row"});
+
+    # work out whether the table needs a class definition set
+    my $class = "";
+    $class = $self -> {"template"} -> load_template("class.tem", {"***class***" -> $self -> {"coursedef"} -> {"table"} -> {"columns"} -> {"class"}})
+        if($self -> {"coursedef"} -> {"table"} -> {"columns"} -> {"class"});
+
+    my $table = $self -> {"template"} -> load_template("table.tem", {"***class***"  => $class,
+                                                                     "***header***" => $header,
+                                                                     "***rows***"   => $body});
+
+    return $self -> {"template"} -> load_template("container.tem", {"***course***" => $self -> {"course"},
+                                                                    "***style***"  => $self -> {"coursedef"} -> {"table"} -> {"style"},
+                                                                    "***table***"  => $table});
+}
+
+
 # ============================================================================
 #  Internal implementation
 
@@ -94,7 +128,7 @@ sub _load_link_data {
 
     $self -> clear_error();
 
-    my $linkfile = path_join($self -> {"basepath"}, "courses", $course, "links.xml");
+    my $linkfile = path_join($self -> {"datapath"}, "courses", $course, "links.xml");
     my $linkdata = eval { XMLin($linkfile, KeepRoot => 0, ContentKey => '-content', KeyAttr => [ 'name' ]) };
     return $self -> self_error("Link file loading failed for $course: $@")
         if($@);
@@ -116,7 +150,7 @@ sub _load_table_data {
 
     $self -> clear_error();
 
-    my $tablefile = path_join($self -> {"basepath"}, "courses", $course, "table.xml");
+    my $tablefile = path_join($self -> {"datapath"}, "courses", $course, "table.xml");
     my $tabledata = eval { XMLin($tablefile, KeepRoot => 0, ForceArray => [ 'column' ], KeyAttr => { 'column' => '', data => 'for' }) };
     return $self -> self_error("Table definition loading failed for $course: $@")
         if($@);
@@ -128,6 +162,91 @@ sub _load_table_data {
     }
 
     return $tabledata;
+}
+
+
+## @method private $ _build_header($columns)
+# Generate a table header row using the specified column data to
+# define the values in the header cells.
+#
+# @param columns A refrence to an array of header columns.
+# @return A string containing the header row.
+sub _build_header {
+    my $self    = shift;
+    my $columns = shift;
+    my $header  = "";
+
+    # Generate the contents of the header cells.
+    foreach my $col (@{$columns}) {
+        my $class = "";
+
+        $self -> {"template"} -> load_template("class.tem", "***class***" => $col -> {"class"})
+            if($col -> {"class"});
+
+        $header .= $self -> {"template"} -> load_template("header.tem", {"***class***" => $class,
+                                                                         "***text***"  => $col -> {"title"}});
+    }
+
+    # Wrap the line in a row if there is any content
+    $header = $self -> {"template"} -> load_template("row.tem", {"***class***" => '',
+                                                                 "***cols***"  => $header})
+        if($header);
+
+    return $header;
+}
+
+
+sub _build_body {
+    my $self    = shift;
+    my $columns = shift;
+    my $rows    = shift;
+    my $body    = shift;
+
+    # Precalulate the number of columns to save a bit of time later
+    my $colcount = scalar(@{$columns});
+
+    # Now process each row in turn, going through the list of columns
+    foreach my $rowdata (@{$rows}) {
+        my $rowvalues = {};
+        my $row = "";
+
+        # allow rows to mark themselves as headers
+        my $mode = $rowdata -> {"header"} ? "header.tem" : "cell.tem";
+
+        for(my $colnum = 0; $colnum < $colcount; ++$colnum) {
+            # Pull out some values to help with readability
+            my $column = $columns -> [$colnum];
+            my $data   = $rowdata -> {"data"} -> {$column -> {"id"}};
+            my ($span, $class) = ("", "");
+
+            if($data -> {"span"}) {
+                $span = $self -> {"template"} -> load_template("colspan.tem", {"***span***" => $data -> {"span"}});
+                $colnum += ($data -> {"span"} - 1); # Skip columns to match the span
+            }
+
+            $class = $self -> {"template"} -> load_template("class.tem", {"***class***" => $data -> {"class"}})
+                if($data -> {"class"});
+
+            # Set the values to substitute
+            $rowvalues -> {"text"}  = $data -> {"content"};
+            $rowvalues -> {"span"}  = $span;
+            $rowvalues -> {"class"} = $class;
+
+            $row .= $self -> {"template"} -> load_template($mode, $rowvalues);
+        }
+
+        # Wrap the row up in a row template if needed
+        if($row) {
+            my $rowclass = "";
+            $rowclass = $self -> {"template"} -> load_template("class.tem", {"***class***" => $rowdata -> {"class"}})
+                if($rowdata -> {"class"});
+
+            $body .= $self -> {"template"} -> load_template("row.tem", {"***class***" => $rowclass,
+                                                                        "***cols***"  => $row});
+        }
+    }
+
+    return $body;
 }
 
 1;
